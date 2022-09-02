1b9d0c5b78bac445547f5603342ff30aa56f52fa
Alex Bradley <a.bradley.dm@gmail.com>
2022-09-02 20:12:11 +0100

    Handy modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NpcComponent } from './components/npc/npc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NpcComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
