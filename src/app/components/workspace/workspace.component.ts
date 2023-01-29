f8994cd0cdc03b0a2f1cf6215c53827bfd66fc88
Alex Bradley <a.bradley.dm@gmail.com>
2023-01-29 16:56:51 +0000

    Remove unused service

--- src/app/components/workspace/workspace.component.ts
+++ src/app/components/workspace/workspace.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef } from "@angular/core";
+import { Component, ViewChild, ViewContainerRef } from "@angular/core";
 import { DragulaService } from "ng2-dragula";
 import { Npc } from "src/app/model/npc";
 import { EventManager } from "src/app/service/event.manager";
@@ -15,10 +15,9 @@ export class WorkspaceComponent {
   container!: ViewContainerRef;
   
   constructor(
-    private dragulaService: DragulaService,
-    private componentFactoryResolver: ComponentFactoryResolver
+    private dragulaService: DragulaService 
   ) {
-    dragulaService.createGroup("DRAGGABLE", {
+    this.dragulaService.createGroup("DRAGGABLE", {
       revertOnSpill: true,
       moves: (_el, _container, handle) => {
         return (<Element>handle).className === 'drag-handle';
