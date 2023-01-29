adb591b717cfc6d8d015ff4575eb9587a87578b8
Alex Bradley <a.bradley.dm@gmail.com>
2023-01-29 17:44:06 +0000

    Don't load all templates while generating list

--- src/app/service/npc-template.manager.ts
+++ src/app/service/npc-template.manager.ts
@@ -6,6 +6,7 @@ export class NpcTemplateManager {
   private static loadedTemplates: {[key: string]: NpcTemplate} = {};
   
   static save(template: NpcTemplate) {
+    this.loadedTemplates[template.key] = template; // update internally (for new templates)
     localStorage.setItem(template.key, JSON.stringify(template));
   }
   
@@ -15,14 +16,16 @@ export class NpcTemplateManager {
     if (template) return template;
     
     // Otherwise check in local storage
-    const str = localStorage.getItem(key);
-    if (!str) throw new Error('No template found with key \'' + key + '\'');
-    
-    // Process the saved data
-    const obj = JSON.parse(str);
-    template = new NpcTemplate(obj.key, obj.name, obj.data);
+    template = this.loadFromLocalStorage(key);
     this.loadedTemplates[key] = template; // store internally
     return template;
+  }
+  
+  private static loadFromLocalStorage(key: string): NpcTemplate {
+    const str = localStorage.getItem(key);
+    if (!str) throw new Error('No template found with key \'' + key + '\'');
+    const obj = JSON.parse(str);
+    return new NpcTemplate(obj.key, obj.name, obj.data);
   }
   
   static delete(key: string) {
@@ -33,7 +36,7 @@ export class NpcTemplateManager {
   static list(): Array<Identifier> {
     const list: Array<Identifier> = [];
     for (let key of Object.keys(localStorage)) {
-      const template: NpcTemplate = this.load(key);
+      const template: NpcTemplate = this.loadFromLocalStorage(key);
       list.push(new Identifier(key, template.name));
     }
     return list;
