263349a00f46d44afa6ce8aacfcbb9f7e11d9e59
Alex Bradley <a.bradley.dm@gmail.com>
2023-01-30 23:54:23 +0000

    Npc Component tweaks

--- src/app/components/npc/npc.component.ts
+++ src/app/components/npc/npc.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from "@angular/core";
+import { map, mergeMap, Observable } from "rxjs";
 import { ValueRequestDialog } from "src/app/dialog/value-request/value-request.dialog";
 import { AttributeEnum } from "src/app/model/enum/attribute.enum";
+import { Property } from "src/app/model/lookup/property";
 import { Npc } from "src/app/model/npc";
 
 @Component({
@@ -10,24 +12,26 @@ import { Npc } from "src/app/model/npc";
 })
 export class NpcComponent {
   
-  attributeEnum = AttributeEnum;
+  readonly attributeEnum = AttributeEnum;
   
-  deleted: boolean = false;
-
   @Input()
   npc!: Npc;
+  
+  deleted: boolean = false;
+  
+  statDisplay(property: Property): Observable<string> {
+    return this.npc.getStatMax(property).pipe(mergeMap(statMax => {
+      return this.npc.getStat(property).pipe(map(stat => {
+        return '' + stat + '/' + statMax;
+      }));
+    }));
+  }
 
   addHp() {
     ValueRequestDialog.requestValue(this.npc, AttributeEnum.HP, Number.parseInt).subscribe(value => {
       this.npc.getStat(AttributeEnum.HP).subscribe(currentHp => {
         this.npc.writeData(AttributeEnum.HP, currentHp + value);
       });
-    });
-  }
-  
-  editMaxHp() {
-    ValueRequestDialog.requestValue(this.npc, AttributeEnum.HP, Number.parseInt).subscribe(value => {
-      this.npc.template.writeData(AttributeEnum.HP, value);
     });
   }
   
