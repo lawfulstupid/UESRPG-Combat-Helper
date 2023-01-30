89515cbedae7b1ba7d6aa75f9c7bb00bc54b769d
Alex Bradley <a.bradley.dm@gmail.com>
2023-01-30 22:07:07 +0000

    Auto-add bandit for testing (REMOVE THIS LATER)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { timeInterval, timeout } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { EventManager } from './service/event.manager';
+import { NpcManager } from './service/npc.manager';
 import { StaticProvider } from './service/static.provider';
 
 @Component({
@@ -8,11 +12,20 @@ import { StaticProvider } from './service/static.provider';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+  autoAdd: boolean = true;
 
   constructor(
     private dialog: MatDialog
   ) {
     StaticProvider.dialog = dialog;
+    if (!environment.production) {
+      if (this.autoAdd) {
+        setTimeout(() => {
+          EventManager.addNpcEvent.emit(NpcManager.create('Testificate', 'bandit'));
+        }, 100);
+      }
+    }
   }
 
 }
