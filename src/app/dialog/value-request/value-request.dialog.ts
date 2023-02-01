7b9b080c860c0fd1fc1f5dad09a37f3788b66f1f
Alex Bradley <a.bradley.dm@gmail.com>
2023-02-01 20:59:30 +0000

    USe property-input on value-request dialog

--- src/app/dialog/value-request/value-request.dialog.ts
+++ src/app/dialog/value-request/value-request.dialog.ts
@@ -14,30 +14,41 @@ import { StaticProvider } from 'src/app/service/static.provider';
 @Component({
   templateUrl: 'value-request.dialog.html'
 })
-export class ValueRequestDialog {
+export class ValueRequestDialog<T> {
 
   actions: Array<ActionItem> = [{
     label: 'Submit',
-    callback: this.respond.bind(this)
+    callback: this.respond.bind(this),
+    isDisabled: this.valid.bind(this)
   }]
   
-  value: string = '';
+  value?: T;
 
   constructor(
-    private dialogRef: MatDialogRef<ValueRequestDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: ValueRequest
+    private dialogRef: MatDialogRef<ValueRequestDialog<T>>,
+    @Inject(MAT_DIALOG_DATA) public data: ValueRequest<T>
   ) {}
+  
+  onValueChange(value: T) {
+    this.value = value;
+  }
+  
+  valid(): boolean {
+    return this.value !== undefined;
+  }
 
   respond() {
-    this.dialogRef.close(this.value);
+    if (this.valid()) {
+      this.dialogRef.close(this.value);
+    }
   }
   
   // performs a request to the user to get a value
   static requestValue<T>(requester: DataCharacter, property: Property<T>): Observable<T> {
     const config = {
-      data: <ValueRequest>{
+      data: <ValueRequest<T>>{
         entityName: requester.name,
-        valueName: property.name
+        property: property
       }
     }
     
@@ -46,7 +57,7 @@ export class ValueRequestDialog {
 
 }
 
-export interface ValueRequest {
+export interface ValueRequest<T> {
   entityName: string;
-  valueName: string;
+  property: Property<T>;
 }
