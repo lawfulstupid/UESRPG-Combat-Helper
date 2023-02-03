5e7ee4e8ddad83509ec05133d34dc90084766688
Alex Bradley <a.bradley.dm@gmail.com>
2023-02-03 23:26:11 +0000

    Default values for SP, AP

--- src/app/model/property/attribute.ts
+++ src/app/model/property/attribute.ts
@@ -8,9 +8,9 @@ export class Attribute extends NumericalProperty {
   public static readonly HP = new Attribute('hitPoints', 'Hit Points', TemplateRole.MAXIMUM);
   public static readonly WT = new Attribute('woundThreshold', 'Wound Threshold', TemplateRole.REFERENCE);
   public static readonly MP = new Attribute('magicka', 'Magicka Points', TemplateRole.MAXIMUM);
-  public static readonly SP = new Attribute('stamina', 'Stamina Points', TemplateRole.MAXIMUM);
+  public static readonly SP = new Attribute('stamina', 'Stamina Points', TemplateRole.MAXIMUM, 3);
   public static readonly IR = new Attribute('initiative', 'Initiative Rating', TemplateRole.REFERENCE);
-  public static readonly AP = new Attribute('actionPoints', 'Action Points', TemplateRole.NO_TEMPLATE);
+  public static readonly AP = new Attribute('actionPoints', 'Action Points', TemplateRole.NO_TEMPLATE, 3);
   public static readonly SPEED = new Attribute('speed', 'Speed', TemplateRole.REFERENCE);
   public static readonly SIZE = new EnumProperty<SizeEnum>('size', 'Size', SizeEnum, TemplateRole.REFERENCE, SizeEnum.STANDARD);
   
