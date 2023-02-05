8f289b6a6b70514c95765a588055c1b467777dc4
Alex Bradley <a.bradley.dm@gmail.com>
2023-02-05 16:42:15 +0000

    Random colour changes on reload

--- src/app/components/menubar/menubar.component.ts
+++ src/app/components/menubar/menubar.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ManageNpcTemplatesDialog } from "src/app/dialog/manage-npc-templates/manage-npc-templates.dialog";
 import { NewNpcTemplateDialog } from "src/app/dialog/new-npc-template/new-npc-template.dialog";
 import { NewNpcDialog } from "src/app/dialog/new-npc/new-npc.dialog";
 import { EventManager } from "src/app/service/event.manager";
 import { StaticProvider } from "src/app/service/static.provider";
+import { RandomUtil } from "src/app/util/random.util";
 import { ActionItem } from "../actionbar/actionbar.component";
 
 @Component({
@@ -11,7 +12,7 @@ import { ActionItem } from "../actionbar/actionbar.component";
   templateUrl: './menubar.component.html',
   styleUrls: ['./menubar.component.scss']
 })
-export class MenubarComponent {
+export class MenubarComponent implements OnInit {
   
   actions: Array<ActionItem> = [{
     label: 'New NPC',
@@ -23,6 +24,14 @@ export class MenubarComponent {
     label: 'Manage Templates',
     callback: this.manageNpcTemplates.bind(this)
   }];
+  
+  ngOnInit() {
+    let [r,g,b] = [0,0,0];
+    while (r+g+b < 100) {
+      [r,g,b] = [RandomUtil.d(255),RandomUtil.d(255),RandomUtil.d(255)];
+    }
+    document.getElementById('menubar')!.style.background = 'rgb(' + r + ',' + g + ',' + b + ')';
+  }
   
   newNpc() {
     StaticProvider.dialog.open(NewNpcDialog).afterClosed().subscribe(npc => {
