8f950b8edfe905b93e1afde2bc32b7e0f4ecc4c3
Alex Bradley <a.bradley.dm@gmail.com>
2023-02-07 17:42:01 +0000

    TextArea Property type

---
+++ src/app/model/property/generic/text-area.property.ts
@@ -0,0 +1,5 @@
+import { TextProperty } from "./text.property";
+
+export class TextAreaProperty extends TextProperty {
+  
+}
--- src/app/model/property/npc.property.ts
+++ src/app/model/property/npc.property.ts
@@ -1,12 +1,12 @@
 import { TemplateRole } from "./abstract/property";
 import { NumericalProperty } from "./generic/number.property";
-import { TextProperty } from "./generic/text.property";
+import { TextAreaProperty } from "./generic/text-area.property";
 
 export class NpcProperties {
   
   /* Misc properties to be held on the Npc and not the NpcTemplate */
   
   public static readonly MISC_MODIFIER = new NumericalProperty('miscModifier', 'Modifier', TemplateRole.NO_TEMPLATE);
-  public static readonly NOTES = new TextProperty('notes', 'Notes', TemplateRole.NO_TEMPLATE);
+  public static readonly NOTES = new TextAreaProperty('notes', 'Notes', TemplateRole.NO_TEMPLATE);
   
 }
