11453ed8c9b457473c5335137132380b0693f595
Alex Bradley <a.bradley.dm@gmail.com>
2023-02-09 22:07:49 +0000

    Remove empty scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { StaticProvider } from './service/static.provider';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   
