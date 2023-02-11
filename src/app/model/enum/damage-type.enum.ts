a5d276b26478889f2af5986dbdf3be6251206ae9
Alex Bradley <a.bradley.dm@gmail.com>
2023-02-11 15:49:01 +0000

    Rename DamageType -> DamageTypeEnum

---
+++ src/app/model/enum/damage-type.enum.ts
@@ -0,0 +1,20 @@
+import { Enum } from "./enum";
+
+export class DamageTypeEnum extends Enum {
+  
+  public static readonly PHYSICAL = new DamageTypeEnum('');
+  public static readonly SLASHING = new DamageTypeEnum('Slashing', this.PHYSICAL);
+  public static readonly SPLITTING = new DamageTypeEnum('Splitting', this.PHYSICAL);
+  public static readonly CRUSHING = new DamageTypeEnum('Crushing', this.PHYSICAL);
+  
+  public static readonly MAGIC = new DamageTypeEnum('Magic');
+  public static readonly FIRE = new DamageTypeEnum('Fire', this.MAGIC);
+  public static readonly FROST = new DamageTypeEnum('Frost', this.MAGIC);
+  public static readonly SHOCK = new DamageTypeEnum('Shock', this.MAGIC);
+  public static readonly POISON = new DamageTypeEnum('Poison', this.MAGIC);
+  
+  constructor(name: string, readonly parent?: DamageTypeEnum) {
+    super(name);
+  }
+  
+}
