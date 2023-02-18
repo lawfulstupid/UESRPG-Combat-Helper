61dc0855f06c0caf5ba8acbbd74dfe04f5ec72e3
Alex Bradley <a.bradley.dm@gmail.com>
2023-02-18 01:46:16 +0000

    Simplify import/export logic by moving serialisation to FileUtil

--- src/app/components/stage/stage.component.ts
+++ src/app/components/stage/stage.component.ts
@@ -1,13 +1,12 @@
-import { Component, ComponentRef, EventEmitter, OnInit, QueryList, ViewChildren, ViewContainerRef } from "@angular/core";
+import { Component, ComponentRef, OnInit, QueryList, ViewChildren, ViewContainerRef } from "@angular/core";
 import { MatDialogConfig } from "@angular/material/dialog";
 import { DragulaService } from "ng2-dragula";
 import { EMPTY, mergeMap, Observable, of } from "rxjs";
 import { ConfirmDialog } from "src/app/dialog/confirm/confirm.dialog";
 import { Npc } from "src/app/model/character/npc";
-import { Persistence } from "src/app/model/serialisation/persistence";
 import { EventManager } from "src/app/service/event.manager";
 import { StaticProvider } from "src/app/service/static.provider";
-import { ErrorComponent } from "../error/error.component";
+import { FileUtil } from "src/app/util/file.util";
 import { NpcComponent } from "../npc/npc.component";
 
 @Component({
@@ -93,57 +92,28 @@ export class StageComponent implements OnInit {
   }
   
   private exportStage() {
-    const stage: Array<Array<any>> = [];
+    const stage: Array<Array<Npc>> = [];
     for (let elmDragColumn of Array.from(document.getElementsByClassName('drag-column'))) {
-      const column: Array<any> = [];
+      const column: Array<Npc> = [];
       for (let elmNpc of Array.from(elmDragColumn.querySelectorAll('app-npc'))) {
         const npc = this.componentRefs.find(ref => ref.location.nativeElement === elmNpc)!.instance.npc;
-        column.push(Persistence.simplify(npc));
+        column.push(npc);
       }
       stage.push(column);
     }
-    this.download(stage, 'stage-' + new Date().toISOString().slice(0,-5) + '.json');
+    FileUtil.download(stage, 'stage-' + new Date().toISOString().slice(0,-5) + '.json');
   }
   
   private importStage() {
-    this.upload<Array<Array<any>>>().subscribe(stage => {
+    FileUtil.upload<Array<Array<Npc>>>().subscribe(stage => {
       this.clearStage().subscribe(() => {
         for (let columnIdx = 0; columnIdx < stage.length; columnIdx++) {
           for (let npc of stage[columnIdx]) {
-            this.addNpc(Persistence.desimplify(npc), columnIdx);
+            this.addNpc(npc, columnIdx);
           }
         }
       });
     });
-  }
-  
-  private download(data: any, filename: string) {
-    const blob = new Blob([JSON.stringify(data)], {type: 'text/json'});
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = filename;
-    link.click();
-    link.remove();
-  }
-  
-  private upload<T>(): Observable<T> {
-    const tempEvent: EventEmitter<T> = new EventEmitter();
-    const link = document.createElement('input');
-    link.type = 'file';
-    link.accept = '.json';
-    link.onchange = () => {
-      link.files?.item(0)?.text().then(text => {
-        try {
-          tempEvent.emit(JSON.parse(text));
-        } catch (e) {
-          ErrorComponent.error('Failed to parse file');
-          throw e;
-        }
-      });
-    };
-    link.click();
-    link.remove();
-    return tempEvent;
   }
   
 }
