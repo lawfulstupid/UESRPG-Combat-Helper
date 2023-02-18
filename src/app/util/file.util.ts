85bee2a77322365eb6e03631f4b442b51590aff3
Alex Bradley <a.bradley.dm@gmail.com>
2023-02-18 01:45:54 +0000

    Move upload/download to FileUtil

---
+++ src/app/util/file.util.ts
@@ -0,0 +1,38 @@
+import { EventEmitter } from "@angular/core";
+import { Observable } from "rxjs";
+import { ErrorComponent } from "../components/error/error.component";
+import { Persistence } from "../model/serialisation/persistence";
+import { PersistableType } from "../model/serialisation/types";
+
+export class FileUtil {
+  
+  static download(data: PersistableType, filename: string) {
+    const blob = new Blob([Persistence.serialise(data)], {type: 'text/json'});
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+    link.remove();
+  }
+  
+  static upload<T extends PersistableType>(): Observable<T> {
+    const tempEvent: EventEmitter<T> = new EventEmitter();
+    const link = document.createElement('input');
+    link.type = 'file';
+    link.accept = '.json';
+    link.onchange = () => {
+      link.files?.item(0)?.text().then(text => {
+        try {
+          tempEvent.emit(Persistence.deserialise(text));
+        } catch (e) {
+          ErrorComponent.error('Failed to parse file');
+          throw e;
+        }
+      });
+    };
+    link.click();
+    link.remove();
+    return tempEvent;
+  }
+  
+}
