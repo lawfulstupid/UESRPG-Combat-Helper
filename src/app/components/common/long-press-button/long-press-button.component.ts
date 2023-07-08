13b01c1ca0b1a25ede73464eac43c17590d3819f
Alex Bradley <a.bradley.dm@gmail.com>
2023-07-08 18:51:55 +0100

    Change long press to continuous press

--- src/app/components/common/long-press-button/long-press-button.component.ts
+++ src/app/components/common/long-press-button/long-press-button.component.ts
@@ -7,9 +7,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class LongPressButtonComponent {
   
   private static readonly LONG_PRESS_DURATION_MS = 500;
+  private static readonly CTS_PRESS_FREQUENCY_MS = 125;
   
   @Input()
   disabled?: boolean | null = false;
+  
+  @Input()
+  longPressMode: 'once' | 'continuous' = 'once';
   
   @Output()
   shortPress: EventEmitter<void> = new EventEmitter();
@@ -17,16 +21,29 @@ export class LongPressButtonComponent {
   @Output()
   longPress: EventEmitter<void> = new EventEmitter();
   
+  @Output()
+  continuousPress: EventEmitter<void> = new EventEmitter();
+  
   longPressTimeout?: NodeJS.Timeout;
+  ctsPressInterval?: NodeJS.Timeout;
   depressed: boolean = false; // is button currently being clicked?
+  deciding: boolean = false; // are we currently deciding what type of click is happening?
 
   onMouseDown() {
     if (this.disabled) return;
+    this.deciding = true;
     this.depressed = true;
     this.longPressTimeout = setTimeout(() => { // this callback happens on long press
-      if (this.depressed) {     // if button is still being held down (not released by leaving)
-        this.depressed = false; // release button
-        this.longPress.emit();  // emit long press
+      if (this.deciding) { // if no decision has been made by this point, short press didn't happen
+        this.deciding = false;
+        if (this.longPressMode === 'once') {
+          this.depressed = false; // release button
+          this.longPress.emit(); // emit long press
+        } else {
+          this.ctsPressInterval = setInterval(() => {
+            this.continuousPress.emit();
+          }, LongPressButtonComponent.CTS_PRESS_FREQUENCY_MS);
+        }
       }
     }, LongPressButtonComponent.LONG_PRESS_DURATION_MS);
   }
@@ -34,16 +51,19 @@ export class LongPressButtonComponent {
   onMouseUp() {
     if (this.disabled) return;
     clearTimeout(this.longPressTimeout); // prevent long press from firing
-    if (this.depressed) {     // if button still being held down (not released by long press or leaving)
-      this.shortPress.emit(); // emit a short press
+    clearTimeout(this.ctsPressInterval); // prevent cts press from firing
+    if (this.deciding) { // if mouse released before a decision is made, it must be a short press
+      this.shortPress.emit();
     }
-    this.depressed = false;   // release button either way
+    this.depressed = false; // release button either way
   }
   
   onMouseLeave() {
     if (this.disabled) return;
-    clearTimeout(this.longPressTimeout);  // prevent long press from firing
-    this.depressed = false;               // release button
+    clearTimeout(this.longPressTimeout); // prevent long press from firing
+    clearTimeout(this.ctsPressInterval); // prevent cts press from firing
+    this.deciding = false; // stop decision making
+    this.depressed = false; // release button
   }
   
 }
